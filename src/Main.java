99ff3fe500a4990c9cac0cf9a25d53161c215796
Saadvikk <saadvik@outlook.com>
2023-09-18 21:49:49 -0400

    lab 05 question 3

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,42 @@
+import java.util.Scanner;
+
 public class Main
 {
     public static void main(String[] args)
     {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Choose your party (case sensitive):");
+        System.out.println("D - democrat");
+        System.out.println("R - republican");
+        System.out.println("I - independent");
+        System.out.print("Other Choice: ");
+
+        char choice = scanner.next().charAt(0);
+
+        if (choice == 'D')
+        {
+            System.out.println("You get a Democratic Donkey! ");
+        }
+
+        else if (choice == 'R')
+        {
+            System.out.println("You get a Republican Elephant! ");
+        }
+
+        else if (choice == 'I')
+        {
+            System.out.println("You get an Independent Person! ");
+        }
+
+        else
+        {
+            System.out.println("You get an other party! ");
+        }
+
+        scanner.close();
+
+
+
+
     }
 }
